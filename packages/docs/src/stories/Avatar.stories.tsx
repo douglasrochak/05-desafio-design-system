929ed5551d79babb2610b7bd9542fcfaaf1d7a63
meamedemontao <douglas.srochak@gmail.com>
2023-01-26 17:18:33 -0300

    ci: Add TurboRep cache on deploy build

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Avatar, AvatarProps } from '@ignite-ui/react'
+import { Avatar, AvatarProps } from '@doro-ui/react'
 
 export default {
   title: 'Typography/Avatar',
--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Button, ButtonProps, Text } from '@ignite-ui/react'
+import { Button, ButtonProps, Text } from '@doro-ui/react'
 import { ArrowRight } from 'phosphor-react'
 
 export default {
