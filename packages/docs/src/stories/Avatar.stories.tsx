b06d1a0761b8e8bbdc42b335a3c5875b64b09b0d
meamedemontao <douglas.srochak@gmail.com>
2023-01-26 13:40:24 -0300

    configurado argsType dos componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -4,17 +4,26 @@ import { Avatar, AvatarProps } from '@ignite-ui/react'
 export default {
   title: 'Typography/Avatar',
   component: Avatar,
+
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+    alt: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj = {
   args: {
     src: 'https://github.com/douglasrochak.png',
     alt: 'Douglas Rochak',
   },
-} as Meta<AvatarProps>
-
-export const Primary: StoryObj = {}
-
-export const WithFallback: StoryObj = {
-  args: {
-    src: undefined,
-    alt: '',
-  },
 }
+
+export const WithFallback: StoryObj = {}
